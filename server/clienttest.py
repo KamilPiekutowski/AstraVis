982f85170961d39dcd68d326e8beb516f113a84c
KamilPiekutowski <kpieku2@uic.edu>
2015-07-29 13:45:09 -0500

    server testing

---
+++ server/clienttest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(('localhost', 8089))
+clientsocket.send('hello')
---
+++ server/runporthole.py
@@ -0,0 +1,16 @@
+from omega import *
+from cyclops import *
+from porthole import *
+from omegaToolkit import *
+from math import *
+import sys
+import porthole
+from webView import *
+
+#ww = webView.create(300,200)
+
+
+#url = 'http//localhost:8081'
+porthole.initialize(8081,'index2.html')
+#porthole.getService().setServerStartedcommand('ww.loadUrl(url)')
+#ww.loadUrl(url)
