4d974fda7ca4b981ca2c340c979cec7c595908ec
KamilPiekutowski <kpieku2@uic.edu>
2015-08-12 13:30:57 -0500

    add functionality to server testing files

--- server/clienttest.py
+++ server/clienttest.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 import socket
+import sys
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket.connect(('localhost', 8089))
-clientsocket.send('hello')
+if len(sys.argv) < 2:
+	clientsocket.send('quit')
+else:
+	clientsocket.send(sys.argv[1])
+
